dfc78e037ed94f93f0895e760b4a839f15826d10
Daniel Subiabre <subiabrewd@gmail.com>
2021-06-02 23:30:16 +0200

    Add desc command

---
+++ src/Command/DescribeBookCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Book;
+use App\Table\BooksTable;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DescribeBookCommand extends AbstractBookCommand
+{
+    protected function configure()
+    {
+        $this->setName('account:describe:book');
+        $this->setAliases(['desc']);
+        $this->setDescription('Show data from a book');
+    
+        $this->addArgument('name', InputArgument::REQUIRED, 'Book name to be read');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument('name');
+
+        $book = $this->bookService->findBookByName($name);
+
+        if (!$book) {
+            $output->writeln(sprintf(Book::MESSAGE_MISSING, $name));
+            return self::FAILURE;
+        }
+
+        $table = new BooksTable($output);
+        $table
+            ->setBooks([$book])
+            ->render();
+
+        return self::SUCCESS;
+    }
+}
