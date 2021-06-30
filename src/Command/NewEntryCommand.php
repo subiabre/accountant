77604e57b66910809e0fb1d29d55215de217e136
Daniel Subiabre <subiabrewd@gmail.com>
2021-06-30 23:59:43 +0200

    Fix class name

--- src/Command/NewEntryCommand.php
+++ src/Command/NewEntryCommand.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class NewBuyCommand extends AbstractBookCommand
+class NewEntryCommand extends AbstractBookCommand
 {
     protected function configure()
     {
